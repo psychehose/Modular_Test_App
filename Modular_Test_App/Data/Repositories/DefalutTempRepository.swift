f7a4bb513200cd00ee92c98f1aea6e0d97e5a894
psychehose <psychehose@gmail.com>
2021-11-26 17:09:41 +0900

    foldering

---
+++ Modular_Test_App/Data/Repositories/DefalutTempRepository.swift
@@ -0,0 +1,8 @@
+//
+//  DefalutTempRepository.swift
+//  Modular_Test_App
+//
+//  Created by 호세김 on 2021/11/26.
+//
+
+import Foundation
---
+++ Modular_Test_App/Domain/Entities/TempEntity.swift
@@ -0,0 +1,8 @@
+//
+//  TempEntity.swift
+//  Modular_Test_App
+//
+//  Created by 호세김 on 2021/11/26.
+//
+
+import Foundation
